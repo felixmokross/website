11d92d01a328e400e9094fa14ac86f20251bc748
Felix Mokross <felixmokross@gmail.com>
2025-01-25 14:33:06 +0100

    chore: Clean up Next.js frontend files from CMS

--- apps/cms/next.config.js
+++ apps/cms/next.config.js
@@ -1,29 +1,19 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 
-import redirects from "./redirects.js";
-
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
-        const url = new URL(item);
-        console.log(`image remote pattern URL: ${url}`);
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(":", ""),
-        };
-      }),
-    ],
-  },
-  reactStrictMode: true,
-  redirects,
   output: "standalone",
+  async redirects() {
+    return [{ source: "/", destination: "/admin", permanent: false }];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "X-Robots-Tag", value: "noindex, nofollow" }],
+      },
+    ];
+  },
 };
 
 export default withPayload(nextConfig);
--- apps/cms/src/utilities/getURL.ts
+++ apps/cms/src/utilities/getURL.ts
@@ -1,5 +1,3 @@
-import canUseDOM from "./canUseDOM";
-
 export const getServerSideURL = () => {
   let url = process.env.NEXT_PUBLIC_SERVER_URL;
 
@@ -12,20 +10,4 @@ export const getServerSideURL = () => {
   }
 
   return url;
-};
-
-export const getClientSideURL = () => {
-  if (canUseDOM) {
-    const protocol = window.location.protocol;
-    const domain = window.location.hostname;
-    const port = window.location.port;
-
-    return `${protocol}//${domain}${port ? `:${port}` : ""}`;
-  }
-
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
-  }
-
-  return process.env.NEXT_PUBLIC_SERVER_URL || "";
 };
