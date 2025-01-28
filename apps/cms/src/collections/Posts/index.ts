c5e02e5d4a8884f38a5532bf42ed8c1ffc0b0b96
Felix Mokross <felixmokross@gmail.com>
2025-01-28 21:08:15 +0100

    Fix build

--- apps/cms/src/collections/Posts/index.ts
+++ apps/cms/src/collections/Posts/index.ts
@@ -2,9 +2,6 @@ import type { CollectionConfig } from "payload";
 
 import { authenticated } from "../../access/authenticated";
 import { authenticatedOrPublished } from "../../access/authenticatedOrPublished";
-import { Banner } from "../../blocks/Banner/config";
-import { Code } from "../../blocks/Code/config";
-import { MediaBlock } from "../../blocks/MediaBlock/config";
 import { generatePreviewPath } from "../../utilities/generatePreviewPath";
 
 import {
