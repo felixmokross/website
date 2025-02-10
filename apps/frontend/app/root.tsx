5f2644ef46aad5c2aa4c08d89b00d9719f048a1d
Felix Mokross <felixmokross@gmail.com>
2025-02-10 19:28:06 +0100

    feat(frontend): Set up favicon

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -28,6 +28,34 @@ export const links: Route.LinksFunction = () => [
   },
   { rel: "stylesheet", href: appStylesheet },
   { rel: "stylesheet", href: prismStylesheet },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "96x96",
+    href: `/favicon-96x96.png`,
+  },
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: `/favicon.svg`,
+  },
+  {
+    rel: "shortcut icon",
+    href: `/favicon.ico`,
+  },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: `/apple-touch-icon.png`,
+  },
+  {
+    rel: "manifest",
+    href: `/site.webmanifest`,
+  },
+];
+
+export const meta: Route.MetaFunction = () => [
+  { name: "apple-mobile-web-app-title", content: "fxmk.dev" },
 ];
 
 export async function loader() {
