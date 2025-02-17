5dfa528f1ca3183594d1546a604a00fe3c5877da
Felix Mokross <felixmokross@gmail.com>
2025-02-17 21:53:49 +0100

    feat(seo): Add sitemap and robots

--- apps/cms/src/collections/Pages/index.ts
+++ apps/cms/src/collections/Pages/index.ts
@@ -1,7 +1,6 @@
 import type { CollectionConfig } from "payload";
 
 import { authenticated } from "../../access/authenticated";
-import { authenticatedOrPublished } from "../../access/authenticatedOrPublished";
 import { Archive } from "../../blocks/ArchiveBlock/config";
 import { CallToAction } from "../../blocks/CallToAction/config";
 import { Content } from "../../blocks/Content/config";
@@ -31,7 +30,7 @@ export const Pages: CollectionConfig<"pages"> = {
   access: {
     create: authenticated,
     delete: authenticated,
-    read: authenticatedOrPublished,
+    read: authenticated,
     update: authenticated,
   },
   // This config controls what's populated by default when a page is referenced
--- apps/frontend/app/routes.ts
+++ apps/frontend/app/routes.ts
@@ -5,4 +5,6 @@ export default [
   route("*", "routes/page/route.tsx"),
   route("/purge-cache", "routes/purge-cache/route.ts"),
   route("/articles/:slug", "routes/post/route.tsx"),
+  route("/sitemap.xml", "routes/sitemap/route.ts"),
+  route("/robots.txt", "routes/robots/route.ts"),
 ] satisfies RouteConfig;
