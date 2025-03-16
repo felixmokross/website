86a43edb3330200b7c828549b7bf5d4b810eb10c
Felix Mokross <felixmokross@gmail.com>
2025-03-16 23:04:13 +0100

    feat(frontend): Improve navigation UX by adding a navigation loading indicator

---
+++ apps/frontend/app/layout/navigation-bar.tsx
@@ -0,0 +1,46 @@
+import { AnimatePresence, motion } from "motion/react";
+import { useState, useEffect } from "react";
+import { useNavigation } from "react-router";
+
+export function NavigationBar() {
+  const navigation = useNavigation();
+  const isDelayedNavigating = useDelayedTrigger(
+    navigation.state !== "idle",
+    100,
+  );
+  return (
+    <AnimatePresence>
+      {isDelayedNavigating && (
+        <motion.div
+          className="fixed top-0 right-0 left-0 z-50 h-0.5 animate-pulse rounded-r-full bg-teal-400 dark:bg-white"
+          initial={{ width: "0%" }}
+          animate={{ width: "80%" }}
+          exit={{
+            width: "100%",
+            transition: { duration: 0.4, opacity: 0, ease: "easeIn" },
+          }}
+          transition={{ duration: 2, ease: "easeOut" }}
+        />
+      )}
+    </AnimatePresence>
+  );
+}
+
+function useDelayedTrigger(isTriggered: boolean, delayInMs: number) {
+  const [isDelayedTriggered, setIsDelayedTriggered] = useState(isTriggered);
+  useEffect(() => {
+    let timeout: number | undefined;
+
+    if (isTriggered) {
+      timeout = window.setTimeout(() => setIsDelayedTriggered(true), delayInMs);
+    } else {
+      setIsDelayedTriggered(false);
+    }
+
+    return () => {
+      if (timeout) window.clearTimeout(timeout);
+    };
+  }, [isTriggered, delayInMs]);
+
+  return isDelayedTriggered;
+}
