7b462739ed909c0dc2160b10bfcaa397b0c5251d
Felix Mokross <felixmokross@gmail.com>
2025-03-22 13:56:14 +0100

    fix: Minor Loading Bar improvement

--- apps/frontend/app/layout/loading-bar.tsx
+++ apps/frontend/app/layout/loading-bar.tsx
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from "motion/react";
 import { useState, useEffect } from "react";
 import { useNavigation } from "react-router";
 
-export function NavigationBar() {
+export function LoadingBar() {
   const navigation = useNavigation();
   const isDelayedNavigating = useDelayedTrigger(
     navigation.state !== "idle",
@@ -12,7 +12,7 @@ export function NavigationBar() {
     <AnimatePresence>
       {isDelayedNavigating && (
         <motion.div
-          className="fixed top-0 right-0 left-0 z-50 h-0.5 animate-pulse rounded-r-full bg-teal-500 dark:bg-teal-400"
+          className="fixed top-0 right-0 left-0 z-50 h-[3px] animate-pulse rounded-r-full bg-teal-500 dark:bg-teal-400"
           initial={{ width: "0%" }}
           animate={{ width: "80%" }}
           exit={{
