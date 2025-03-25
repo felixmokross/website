1a20036db486d1b097683a663c2c82ec63ad2468
Felix Mokross <felixmokross@gmail.com>
2025-03-25 14:53:57 +0100

    cicd: Extend releaser to create a GitHub Release and run Build workflow after version tag has been created

--- tools/releaser/src/index.ts
+++ tools/releaser/src/index.ts
@@ -1,12 +1,13 @@
 import {
-  ChangelogConfig,
   determineSemverChange,
   getGitDiff,
   parseCommits,
   loadChangelogConfig,
+  generateMarkDown,
 } from "changelogen";
 import { execSync } from "child_process";
 import semver from "semver";
+import { Octokit } from "@octokit/rest";
 
 function getLatestGitTag(): string {
   const tag = execSync("git describe --tags --abbrev=0").toString().trim();
@@ -17,7 +18,9 @@ const lastVersionTag = getLatestGitTag();
 const lastVersion = lastVersionTag.substring(1);
 console.log(`Last version was: ${lastVersion}`);
 
-const config: ChangelogConfig = await loadChangelogConfig(process.cwd());
+const config = await loadChangelogConfig(process.cwd(), {
+  from: lastVersionTag,
+});
 
 const gitCommits = parseCommits(await getGitDiff(lastVersionTag), config);
 
@@ -25,8 +28,27 @@ const bump = determineSemverChange(gitCommits, config);
 if (!bump) throw new Error("Version bump could not be determined");
 
 const newVersion = semver.inc(lastVersion, bump);
-console.log(`New version will be: ${newVersion}`);
+if (!newVersion) throw new Error("New version could not be determined");
 
+const newVersionTag = `v${newVersion}`;
+config.to = newVersionTag;
+
+console.log(`Version: ${newVersion}`);
 console.log(`Tagging and pushing…`);
-execSync(`git tag v${newVersion}`);
-execSync(`git push origin v${newVersion}`);
+
+execSync(`git tag ${newVersionTag}`);
+execSync(`git push origin ${newVersionTag}`);
+
+console.log(`Creating release…`);
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+const release = await octokit.repos.createRelease({
+  owner: "felixmokross",
+  repo: "website",
+  tag_name: newVersionTag,
+  name: newVersionTag,
+  body: await generateMarkDown(gitCommits, config),
+  draft: false,
+  prerelease: false,
+});
+
+console.log("Release created:", release.data.html_url);
