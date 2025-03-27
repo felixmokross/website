7b9b290c2e05945977d4e5c9f4f01b2a61dcbe5e
Felix Mokross <felixmokross@gmail.com>
2025-03-27 20:01:11 +0100

    Simplify and improve code

--- tools/releaser/src/commands/get-version.ts
+++ tools/releaser/src/commands/get-version.ts
@@ -2,27 +2,28 @@ import { determineSemverChange } from "changelogen";
 import { execSync } from "child_process";
 import { program } from "commander";
 import semver from "semver";
-import { getConfig, getGitCommits, getLastVersionTag } from "src/common";
+import {
+  filterConventionalCommits,
+  getConfig,
+  getRawCommits,
+  getLastVersionTag,
+} from "src/common";
 
 program.command("get-version").action(async () => {
-  const branchName = execSync(`git rev-parse --abbrev-ref HEAD`)
-    .toString()
-    .trim();
-
   const lastVersionTag = getLastVersionTag();
   const config = await getConfig(lastVersionTag);
-  const gitCommits = await getGitCommits(lastVersionTag, config);
+  const rawCommits = await getRawCommits(lastVersionTag);
 
   const lastVersion = lastVersionTag.substring(1);
 
   let newVersion = lastVersion;
 
-  const sha = execSync(`git rev-parse --short HEAD`).toString().trim();
-
-  const hasUnreleasedChanges = gitCommits.length > 0;
+  const hasUnreleasedChanges = rawCommits.length > 0;
   if (hasUnreleasedChanges) {
     // At least the patch version will increase if there are any new commits
-    const bumpType = determineSemverChange(gitCommits, config) ?? "patch";
+    const conventionalCommits = filterConventionalCommits(rawCommits, config);
+    const bumpType =
+      determineSemverChange(conventionalCommits, config) ?? "patch";
     const bumpedVersion = semver.inc(lastVersion, bumpType);
     if (!bumpedVersion) {
       throw new Error("New version could not be determined");
@@ -30,20 +31,19 @@ program.command("get-version").action(async () => {
 
     newVersion = bumpedVersion;
 
-    const commitsSinceLastVersion = execSync(
-      `git rev-list --count ${lastVersionTag}..HEAD`,
-    )
+    const githubHeadRef = process.env.GITHUB_HEAD_REF;
+    const branchName = execSync(`git rev-parse --abbrev-ref HEAD`)
       .toString()
       .trim();
-
-    const githubHeadRef = process.env.GITHUB_HEAD_REF;
     const sanitizedBranchName = (githubHeadRef || branchName)
       .replace("/", "-")
       .replace("_", "-")
       .replace(/[^a-zA-Z0-9.-]/g, "");
 
-    newVersion += `-${sanitizedBranchName}.${commitsSinceLastVersion}`;
+    newVersion += `-${sanitizedBranchName}.${rawCommits.length}`;
   }
+
+  const sha = execSync(`git rev-parse --short HEAD`).toString().trim();
 
   console.log(
     JSON.stringify(
