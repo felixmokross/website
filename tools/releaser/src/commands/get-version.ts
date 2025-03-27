dfa838feada9e66f51fa69be135d9d09f09f8b5c
Felix Mokross <felixmokross@gmail.com>
2025-03-27 19:22:56 +0100

    cicd: Split Build and Deploy workflows

--- tools/releaser/src/commands/get-version.ts
+++ tools/releaser/src/commands/get-version.ts
@@ -4,63 +4,55 @@ import { program } from "commander";
 import semver from "semver";
 import { getConfig, getGitCommits, getLastVersionTag } from "src/common";
 
-program
-  .command("get-version")
-  .option("-r, --release", "Create a release version")
-  .action(async (options) => {
-    const commitMessage = execSync(`git log -1 --pretty=%B`).toString().trim();
-    const branchName = execSync(`git rev-parse --abbrev-ref HEAD`)
+program.command("get-version").action(async () => {
+  const branchName = execSync(`git rev-parse --abbrev-ref HEAD`)
+    .toString()
+    .trim();
+
+  const lastVersionTag = getLastVersionTag();
+  const config = await getConfig(lastVersionTag);
+  const gitCommits = await getGitCommits(lastVersionTag, config);
+
+  const lastVersion = lastVersionTag.substring(1);
+
+  let newVersion = lastVersion;
+
+  const sha = execSync(`git rev-parse --short HEAD`).toString().trim();
+
+  const hasUnreleasedChanges = gitCommits.length > 0;
+  if (hasUnreleasedChanges) {
+    // At least the patch version will increase if there are any new commits
+    const bumpType = determineSemverChange(gitCommits, config) ?? "patch";
+    const bumpedVersion = semver.inc(lastVersion, bumpType);
+    if (!bumpedVersion) {
+      throw new Error("New version could not be determined");
+    }
+
+    newVersion = bumpedVersion;
+
+    const commitsSinceLastVersion = execSync(
+      `git rev-list --count ${lastVersionTag}..HEAD`,
+    )
       .toString()
       .trim();
 
-    const lastVersionTag = getLastVersionTag();
-    const config = await getConfig(lastVersionTag);
-    const gitCommits = await getGitCommits(lastVersionTag, config);
+    const githubHeadRef = process.env.GITHUB_HEAD_REF;
+    const sanitizedBranchName = (githubHeadRef || branchName)
+      .replace("/", "-")
+      .replace("_", "-")
+      .replace(/[^a-zA-Z0-9.-]/g, "");
 
-    const lastVersion = lastVersionTag.substring(1);
+    newVersion += `-${sanitizedBranchName}.${commitsSinceLastVersion}`;
+  }
 
-    let newVersion = lastVersion;
-    if (gitCommits.length > 0) {
-      // At least the patch version will increase if there are any new commits
-      const bumpType = determineSemverChange(gitCommits, config) ?? "patch";
-      const bumpedVersion = semver.inc(lastVersion, bumpType);
-      if (!bumpedVersion) {
-        throw new Error("New version could not be determined");
-      }
-
-      newVersion = bumpedVersion;
-    }
-
-    const sha = execSync(`git rev-parse --short HEAD`).toString().trim();
-    const isRelease = commitMessage.includes("!release") || !!options.release;
-
-    let completeNewVersion = newVersion;
-
-    if (!isRelease) {
-      const commitsSinceLastVersion = execSync(
-        `git rev-list --count ${lastVersionTag}..HEAD`,
-      )
-        .toString()
-        .trim();
-
-      const githubHeadRef = process.env.GITHUB_HEAD_REF;
-      const sanitizedBranchName = (githubHeadRef || branchName)
-        .replace("/", "-")
-        .replace("_", "-")
-        .replace(/[^a-zA-Z0-9.-]/g, "");
-
-      completeNewVersion += `-${sanitizedBranchName}.${commitsSinceLastVersion}`;
-    }
-
-    console.log(
-      JSON.stringify(
-        {
-          version: completeNewVersion,
-          versionWithSha: `${completeNewVersion}-${sha}`,
-          isNewRelease: isRelease && newVersion !== lastVersion,
-        },
-        null,
-        2,
-      ),
-    );
-  });
+  console.log(
+    JSON.stringify(
+      {
+        version: newVersion,
+        versionWithSha: `${newVersion}-${sha}`,
+      },
+      null,
+      2,
+    ),
+  );
+});
