91d6125e0b1c2a7a6e38300b6fd3d02a87b31bcc
Felix Mokross <felixmokross@gmail.com>
2025-03-28 20:19:13 +0100

    cicd: Improve get-version command (#63)

--- tools/releaser/src/commands/get-version.ts
+++ tools/releaser/src/commands/get-version.ts
@@ -1,4 +1,8 @@
-import { determineSemverChange } from "changelogen";
+import {
+  determineSemverChange,
+  RawGitCommit,
+  ResolvedChangelogConfig,
+} from "changelogen";
 import { execSync } from "child_process";
 import { program } from "commander";
 import semver from "semver";
@@ -15,56 +19,89 @@ program
   .action(async (options) => {
     const lastVersionTag = getLastVersionTag();
     const config = await getConfig(lastVersionTag);
-    const rawCommits = await getRawCommits(lastVersionTag);
+    const commitsSinceLastVersion = await getRawCommits(lastVersionTag);
 
-    const lastVersion = lastVersionTag.substring(1);
+    const lastVersion = getVersionFromGitTag(lastVersionTag);
 
-    let newVersion = lastVersion;
-
-    const hasUnreleasedChanges = rawCommits.length > 0;
-
+    const hasUnreleasedChanges = commitsSinceLastVersion.length > 0;
     const isReleaseRequested =
-      (hasUnreleasedChanges && rawCommits[0].body.includes("!release")) ||
-      !!options.release;
+      hasReleaseKeyword(commitsSinceLastVersion) || !!options.release;
 
-    const isNewRelease = hasUnreleasedChanges && isReleaseRequested;
-    if (hasUnreleasedChanges) {
-      // At least the patch version will increase if there are any new commits
-      const conventionalCommits = filterConventionalCommits(rawCommits, config);
-      const bumpType =
-        determineSemverChange(conventionalCommits, config) ?? "patch";
-      const bumpedVersion = semver.inc(lastVersion, bumpType);
-      if (!bumpedVersion) {
-        throw new Error("New version could not be determined");
-      }
-
-      newVersion = bumpedVersion;
-
-      if (!isReleaseRequested) {
-        const githubHeadRef = process.env.GITHUB_HEAD_REF;
-        const branchName = execSync(`git rev-parse --abbrev-ref HEAD`)
-          .toString()
-          .trim();
-        const sanitizedBranchName = (githubHeadRef || branchName)
-          .replace("/", "-")
-          .replace("_", "-")
-          .replace(/[^a-zA-Z0-9.-]/g, "");
-
-        newVersion += `-${sanitizedBranchName}.${rawCommits.length}`;
-      }
-    }
-
-    const sha = execSync(`git rev-parse --short HEAD`).toString().trim();
+    const newVersion = getNewVersion(
+      hasUnreleasedChanges,
+      isReleaseRequested,
+      commitsSinceLastVersion,
+      lastVersion,
+      config,
+    );
 
     console.log(
       JSON.stringify(
         {
           version: newVersion,
-          versionWithSha: `${newVersion}-${sha}`,
-          isNewRelease,
+          versionWithSha: `${newVersion}-${getHeadCommitSha()}`,
+          isNewRelease: hasUnreleasedChanges && isReleaseRequested,
         },
         null,
         2,
       ),
     );
   });
+
+function getVersionFromGitTag(tag: string) {
+  return tag.substring(1);
+}
+
+function getHeadCommitSha() {
+  return execSync(`git rev-parse --short HEAD`).toString().trim();
+}
+
+function hasReleaseKeyword(rawCommits: RawGitCommit[]) {
+  return rawCommits.some((c) => c.body.includes("!release"));
+}
+
+function getNewVersion(
+  hasUnreleasedChanges: boolean,
+  isReleaseRequested: boolean,
+  rawCommits: RawGitCommit[],
+  lastVersion: string,
+  config: ResolvedChangelogConfig,
+) {
+  if (!hasUnreleasedChanges) return lastVersion;
+
+  const newReleaseVersion = determineNewReleaseVersion(
+    rawCommits,
+    lastVersion,
+    config,
+  );
+  if (isReleaseRequested) return newReleaseVersion;
+
+  const githubHeadRef = process.env.GITHUB_HEAD_REF;
+  const branchName = execSync(`git rev-parse --abbrev-ref HEAD`)
+    .toString()
+    .trim();
+  const sanitizedBranchName = (githubHeadRef || branchName)
+    .replace("/", "-")
+    .replace("_", "-")
+    .replace(/[^a-zA-Z0-9.-]/g, "");
+
+  return `${newReleaseVersion}-${sanitizedBranchName}.${rawCommits.length}`;
+}
+
+function determineNewReleaseVersion(
+  rawCommits: RawGitCommit[],
+  lastVersion: string,
+  config: ResolvedChangelogConfig,
+) {
+  const conventionalCommits = filterConventionalCommits(rawCommits, config);
+
+  // At least the patch version will increase if there are any new commits
+  const bumpType =
+    determineSemverChange(conventionalCommits, config) ?? "patch";
+  const bumpedVersion = semver.inc(lastVersion, bumpType);
+  if (!bumpedVersion) {
+    throw new Error("New version could not be determined");
+  }
+
+  return bumpedVersion;
+}
