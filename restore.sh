5ea386bb7c9ed619eac579eef30d2af0236c4ad5
Felix Mokross <felixmokross@gmail.com>
2025-03-29 22:24:46 +0100

    ci: Add api-keys to excluded collections in DB restore script

--- restore.sh
+++ restore.sh
@@ -4,12 +4,12 @@
 
 echo "Dropping database…"
 mongosh $RESTORE_DATABASE_URI <<EOF
-db.getCollectionNames().filter(c => !['users', 'payload-preferences'].includes(c)).forEach((c) => { db.getCollection(c).drop(); });
+db.getCollectionNames().filter(c => !['users', 'api-keys', 'payload-preferences'].includes(c)).forEach((c) => { db.getCollection(c).drop(); });
 EOF
 
 echo ""
 echo "Restoring database…"
-mongorestore --uri $RESTORE_DATABASE_URI  --nsExclude $RESTORE_DATABASE_NAME.users --nsExclude $RESTORE_DATABASE_NAME.payload-preferences --drop .backup/payload
+mongorestore --uri $RESTORE_DATABASE_URI  --nsExclude $RESTORE_DATABASE_NAME.users --nsExclude $RESTORE_DATABASE_NAME.api-keys --nsExclude $RESTORE_DATABASE_NAME.payload-preferences --drop .backup/payload
 
 echo "Restoring S3 bucket…"
 aws s3 sync ./.s3-backup $RESTORE_S3_BUCKET --delete --profile website
